7c25bdd0ef7db0146262935144f2097cf01b98f9
Lior Hadashian <lior@gavra-games.com>
2023-04-02 17:41:55 +0300

    2021.3.19f1

--- RepoProjectTiltan/Assets/Scripts/Circle2DMover.cs
+++ RepoProjectTiltan/Assets/Scripts/Circle2DMover.cs
@@ -7,8 +7,14 @@ public class Circle2DMover : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D _rigidbody2D;
     [SerializeField] private float speed;
-    private void FixedUpdate()
+
+    private void Start()
     {
-        _rigidbody2D.AddForce(Vector2.right * (Time.deltaTime * speed), ForceMode2D.Force);
+        _rigidbody2D.AddForce(Vector2.right * speed, ForceMode2D.Impulse);
     }
+
+    // private void FixedUpdate()
+    // {
+    //     _rigidbody2D.AddForce(Vector2.right * (Time.deltaTime * speed), ForceMode2D.Force);
+    // }
 }
