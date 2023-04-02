aecc386dda8999625c7b2bc4ee328a425d37e231
Lior Hadashian <lior@gavra-games.com>
2023-04-02 17:14:57 +0300

    added cool circle and platform

---
+++ RepoProjectTiltan/Assets/Scripts/Circle2DMover.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle2DMover : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _rigidbody2D;
+    [SerializeField] private float speed;
+    private void FixedUpdate()
+    {
+        _rigidbody2D.AddForce(Vector2.right * (Time.deltaTime * speed), ForceMode2D.Force);
+    }
+}
