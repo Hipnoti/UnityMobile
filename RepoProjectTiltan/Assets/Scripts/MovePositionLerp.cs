07699d8d6172f80e68d2aef4ca793a71dc45d7cf
Lior Hadashian <lior@gavra-games.com>
2023-04-30 17:26:48 +0300

    W.I.P Lerps

---
+++ RepoProjectTiltan/Assets/Scripts/MovePositionLerp.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePositionLerp : MonoBehaviour
+{
+
+    [SerializeField] private Transform targetTransform;
+    [SerializeField] private float lerpDuration;
+    
+    private Vector3 startingPosition;
+    private bool movingTowardsTarget = false;
+    
+    private float sumTimePassed = 0;
+    
+    void Start()
+    {
+        startingPosition = transform.position;
+        movingTowardsTarget = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sumTimePassed += Time.deltaTime;
+        if (movingTowardsTarget)
+        {
+           transform.position = Vector3.Lerp(startingPosition, targetTransform.position, sumTimePassed/lerpDuration);
+           if (sumTimePassed >= lerpDuration)
+               movingTowardsTarget = false;
+        }
+    }
+}
